2ba9108fb41b141ebe8b26f2c246cd65cc0a1502
Pavel Hontarenko <pavgont@gmail.com>
2020-02-12 16:24:58 +0200

    added two routes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
     <div id="app">
-        <router-view></router-view> <!--Отображение выбранного компонента-->
-
-
-        <div>Hello, {{ user.name | uppercase }}!</div>
         <!--notification-->
         <notification :message="notify.message" :status="notify.status" v-if="showNotify"
                       @cancel="showNotify = false"></notification>
 
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <router-link exact active-class="font-bold" to="/">Home</router-link>
+        <router-link active-class="font-bold" to="/users">Users</router-link>
 
-        <user-profile :user="user" @save-user="user = $event"></user-profile>
+        <router-view></router-view> <!--Отображение выбранного компонента-->
     </div>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
-    import UserProfile from './components/User/Profile'
     import Notification from './components/Support/Notification'
     import Notifier from './mixins/notifier'
 
@@ -25,20 +19,14 @@
         name: 'app',
         mixins: [Notifier],
         components: {
-            HelloWorld, UserProfile, Notification
+            Notification
         },
         data() {
             return {
-                user: {
-                    name: 'John',
-                    surname: 'Doe',
-                    aboutMe: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-                }
+
             }
         },
-        methods: {
-
-        }
+        methods: {}
     }
 </script>
 
